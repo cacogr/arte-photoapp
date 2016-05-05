77fabdc13447cc4a1f057bd1e8ed72d70a95290c
Carlos Garcia Ramos <cgarram@arte-consultores.com>
2016-05-05 19:42:31 +0100

    List Activity + Detail Fragment. Http Requests.

--- app/src/main/java/com/arte/photoapp/network/GetPhotoListRequest.java
+++ app/src/main/java/com/arte/photoapp/network/GetPhotoListRequest.java
@@ -2,12 +2,24 @@ package com.arte.photoapp.network;
 
 
 import android.content.Context;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.arte.photoapp.model.Photo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class GetPhotoListRequest {
+
+    private static final String PHOTO_LIST_URL = "http://jsonplaceholder.typicode.com/photos";
 
     public interface Callbacks {
         void onGetPhotoListSuccess(List<Photo> photoList);
@@ -24,8 +36,39 @@ public class GetPhotoListRequest {
     }
 
     public void execute() {
-        // TODO do JSONArray volley request
-        // TODO transform JSONArray to List<Photo>
-        // TODO call mCallbacks methods
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, PHOTO_LIST_URL, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                List<Photo> photoList = new ArrayList<>();
+
+                for (int i = 0; i < response.length(); i++) {
+                    Photo photo = new Photo();
+                    try {
+                        JSONObject currentObject = response.getJSONObject(i);
+                        photo.setId("" + currentObject.getInt("id"));
+                        photo.setTitle(currentObject.getString("title"));
+                        photo.setUrl(currentObject.getString("url"));
+                        photo.setThumbnailUrl(currentObject.getString("thumbnailUrl"));
+                    } catch (JSONException e) {
+                        Log.e(GetPhotoListRequest.class.getSimpleName(), "Error deserializando JSON", e);
+                        mCallbacks.onGetPhotoListError();
+                        return;
+                    }
+
+                    photoList.add(photo);
+                }
+
+                mCallbacks.onGetPhotoListSuccess(photoList);
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mCallbacks.onGetPhotoListError();
+            }
+        });
+
+        RequestQueueManager.getInstance(mContext).addToRequestQueue(request);
     }
 }
